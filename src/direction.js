69de26ccd56d719b9cd079ac0c0ba37cd4b4dfa2
Sergio Tabares <alu0101124896@ull.edu.es>
2020-04-18 13:45:00 +0000

    Direction Class structure

---
+++ src/direction.js
@@ -0,0 +1,30 @@
+/**
+ * @file direction.js
+ * @author Sergio Tabares Hernández <alu0101124896@ull.edu.es>
+ * @since Spring 2020
+ * @summary University of La Laguna
+ * @summary Computer Science - Interactive Aplication Programing
+ * @description This program implements a direction class for Random Walk. More info about it here: https://en.wikipedia.org/wiki/Random_walk
+ */
+
+"use strict";
+
+/**
+ * @description Class representing a direction
+ *
+ * @class Direction
+ */
+class Direction {
+
+  /**
+   * @description Constructor that creates an instance of a direction.
+   *
+   * @memberof Direction
+   */
+  constructor() {
+  }
+}
+
+if (typeof exports !== 'undefined') { // Execution in node
+  exports.Direction = Direction;
+}
---
+++ test/direction-test.js
@@ -0,0 +1,26 @@
+/**
+ * @file direction-test.js
+ * @author Sergio Tabares Hernández <alu0101124896@ull.edu.es>
+ * @since Spring 2020
+ * @summary University of La Laguna
+ * @summary Computer Science - Interactive Aplication Programing
+ * @description This program implements the tests for the direction class
+ */
+
+"use strict";
+
+let expectOnDirectionTest;
+let ClassDirectionOnDirectionTest;
+if (typeof require !== 'undefined') { // Execution in node
+  expectOnDirectionTest = require('chai').expect;
+  ClassDirectionOnDirectionTest = require('../src/direction.js').Direction;
+} else { // Execution in browser
+  expectOnDirectionTest = expect;
+  ClassDirectionOnDirectionTest = Direction;
+}
+
+describe('ClassDirectionOnDirectionTest Class', () => {
+  describe('Default properties', () => {
+    const MY_DIRECTION = new ClassDirectionOnDirectionTest();
+  });
+});
