a0c970982969f15fb3e820ba0e4bd8decaa8a17b
Sergio Tabares <alu0101124896@ull.edu.es>
2020-04-18 18:58:28 +0000

    Point xCoord property red

---
+++ src/point.js
@@ -0,0 +1,31 @@
+/**
+ * @file point.js
+ * @author Sergio Tabares Hernández <alu0101124896@ull.edu.es>
+ * @since Spring 2020
+ * @summary University of La Laguna
+ * @summary Computer Science - Interactive Aplication Programing
+ * @description This program implements a point class for Random Walk. More
+ *  info about it here: https://en.wikipedia.org/wiki/Random_walk
+ */
+
+"use strict";
+
+/**
+ * @description Class representing a point
+ *
+ * @class Point
+ */
+class Point {
+
+  /**
+   * @description Constructor that creates an instance of a point.
+   *
+   * @memberof Point
+   */
+  constructor() {
+  }
+}
+
+if (typeof exports !== 'undefined') { // Execution in node
+  exports.Point = Point;
+}
