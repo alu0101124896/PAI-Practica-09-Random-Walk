283628bca330e643cb7dab7dc36332b146602c65
Sergio Tabares <alu0101124896@ull.edu.es>
2020-04-26 20:28:53 +0000

    minor tweak

--- web-server/app.js
+++ web-server/app.js
@@ -8,11 +8,6 @@ APP.set('port', 8080);
 //tell express that we want to use the www folder
 //for our static assets
 APP.use(EXPRESS.static(PATH.join(__dirname, '../www')));
-// APP.use(EXPRESS.static(PATH.join(__dirname, '../coverage')));
-// APP.use(EXPRESS.static(PATH.join(__dirname, '../doc-poker')));
-APP.use(EXPRESS.static(PATH.join(__dirname, '../node_modules')));
-APP.use(EXPRESS.static(PATH.join(__dirname, '../src')));
-APP.use(EXPRESS.static(PATH.join(__dirname, '../test')));
 APP.use(EXPRESS.static(PATH.join(__dirname, '..')));
 
 // Listen for requests
